8b4e2febb970b6429b5e204ae38d9534a535c5a5
Włodzimierz Gajda <gajdaw@gajdaw.pl>
2015-01-13 12:52:06 +0100

    ParseVariableFromFile action

---
+++ spec/Stamp/Action/ParseVariableFromFileSpec.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace spec\Stamp\Action;
+
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+use Stamp\Action\FileReader;
+use Stamp\Action\ParseVariable;
+
+class ParseVariableFromFileSpec extends ObjectBehavior
+{
+
+    function let(FileReader $fileReader, ParseVariable $variableParser)
+    {
+        $this->beConstructedWith($fileReader, $variableParser);
+    }
+
+    function it_is_initializable()
+    {
+        $this->shouldHaveType('Stamp\Action\ParseVariableFromFile');
+    }
+
+    function it_should_parse_a_variable_from_file(FileReader $fileReader, ParseVariable $variableParser)
+    {
+        $text = '
+            "name"  :   "Lorem"
+        ';
+        $params = array(
+            'filename' => 'fake.file.json',
+            'regex' => '/"name" *: *"(?P<name>[^"]+)"/'
+        );
+
+        $fileReader->fileGetContents($params['filename'])->willReturn($text);
+        $variableParser->exec()->willReturn(true);
+        $variableParser->getResult()->willReturn(array('name'=>'Lorem'));
+
+        $this->setParams($params);
+        $variableParser->setParams(array(
+            'text' => $text,
+            'regex' => $params['regex']
+        ))->shouldBeCalled();
+        $variableParser->getResult()->shouldBeCalled();
+
+        $this->exec()->shouldReturn(true);
+        $this->getResult()->shouldReturn(array('name' => 'Lorem'));
+        $this->getOutput()->shouldReturn(null);
+    }
+
+    function it_should_parse_a_variable_from_file_in_verbose_mode(FileReader $fileReader, ParseVariable $variableParser)
+    {
+        $text = '
+            "name"  :   "Lorem"
+        ';
+        $params = array(
+            'filename' => 'f.json',
+            'regex' => '/"name" *: *"(?P<name>[^"]+)"/'
+        );
+
+        $fileReader->fileGetContents($params['filename'])->willReturn($text);
+
+        $variableParser->exec()->willReturn(true);
+        $variableParser->getResult()->willReturn(array('name'=>'Lorem'));
+        $variableParser->setParams(array(
+            'text' => $text,
+            'regex' => $params['regex']
+        ))->shouldBeCalled();
+        $variableParser->getResult()->shouldBeCalled();
+
+
+        $this->setParams($params);
+        $this->setVerbose(true);
+        $this->exec()->shouldReturn(true);
+        $this->getResult()->shouldReturn(array('name' => 'Lorem'));
+        $this->getOutput()->shouldReturn('ParseVariableFromFile["filename"="f.json"]["name"="Lorem"]');
+    }
+}
---
+++ src/Stamp/Action/ParseVariableFromFile.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace Stamp\Action;
+use Stamp\Action\FileReader;
+use Stamp\Action\ParseVariable;
+
+class ParseVariableFromFile
+{
+    private $filename;
+    private $regex;
+    private $fileReader;
+    private $result;
+    private $output;
+    private $verbose = false;
+
+    public function __construct(FileReader $fileReader, ParseVariable $variableParser)
+    {
+        $this->fileReader = $fileReader;
+        $this->variableParser = $variableParser;
+    }
+
+    public function setFilename($filename)
+    {
+        $this->filename = $filename;
+    }
+
+    public function setRegex($regex)
+    {
+        $this->regex = $regex;
+    }
+
+    public function setParams($array)
+    {
+        $this->setFilename($array['filename']);
+        $this->setRegex($array['regex']);
+    }
+
+    public function exec()
+    {
+        $contents = $this->fileReader->fileGetContents($this->filename);
+        $this->variableParser->setParams(array(
+            'text' => $contents,
+            'regex' => $this->regex
+        ));
+
+        $resultToBeReturned = $this->variableParser->exec();
+        $this->result = $this->variableParser->getResult();
+
+        if ($this->verbose) {
+            $key = array_keys($this->result)[0];
+            $this->output = sprintf(
+                'ParseVariableFromFile["filename"="%s"]["%s"="%s"]',
+                $this->filename,
+                $key, $this->result[$key]
+            );
+        }
+
+        return $resultToBeReturned;
+    }
+
+    public function getResult()
+    {
+        return $this->result;
+    }
+
+    public function getOutput()
+    {
+        return $this->output;
+    }
+
+    public function setVerbose($verbose)
+    {
+        $this->verbose = $verbose;
+    }
+}
