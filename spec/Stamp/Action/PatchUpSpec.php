46118ac5fe72f3211f18d7d35d040ffc22c17a1d
Włodzimierz Gajda <gajdaw@gajdaw.pl>
2015-01-14 10:10:07 +0100

    MinorUp and PatchUp missing phpspec tests

---
+++ spec/Stamp/Action/PatchUpSpec.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace spec\Stamp\Action;
+
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+use Stamp\Tools\VersionParser;
+use Stamp\Tools\VariableContainer;
+
+
+class MinorUpSpec extends ObjectBehavior
+{
+    function let(VariableContainer $variableContainer, VersionParser $versionParser)
+    {
+        $this->beConstructedWith($variableContainer, $versionParser);
+    }
+
+    function it_is_initializable()
+    {
+        $this->shouldHaveType('Stamp\Action\MinorUp');
+    }
+    function it_should_parse_version(VariableContainer $variableContainer, VersionParser $versionParser)
+    {
+        $tmpVersion = '3.421.55';
+
+        $variableContainer->getVariable('version')->willReturn('3.421.55');
+        $variableContainer->setVariable('version', '3.421.56')->shouldBeCalled();
+
+        $versionParser->parse($tmpVersion)->willReturn(array(
+           'major' => '3', 'minor' => '421', 'patch' => '55'
+        ));
+
+        $params = array(
+            'variable'  => 'version'
+        );
+        $this->setVerbose(true);
+        $this->setParams($params);
+        $this->exec()->shouldReturn(true);
+        $this->getOutput()->shouldReturn('minor_up["version"="3.421.56"]');
+    }
+}
